5545022d59c18b6392dd556dc0d02d1d3102afbb
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-05 09:49:36 -0700

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             "ace-eye"
+maintainer       "Ace of Sales"
+maintainer_email "cookbooks@aceofsales.com"
+license          "Apache 2.0"
+description      "Installs/Configures eye for aceofsales"
+version          "1.0.0"
+
+depends "eye"
+
+provides "ace-eye"
---
+++ recipes/default.rb
@@ -0,0 +1,3 @@
+node.force_default['eye']['version'] = '0.6'
+node.force_default['eye']['bin'] = '/usr/local/bin/eye'
+include_recipe 'eye'
