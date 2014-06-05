ce254192e31ff7f92599dcce2a294a951082b983
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-05 10:20:06 -0700

    Move attributes to attributes file

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+force_default['eye']['version'] = '0.6'
+force_default['eye']['bin'] = '/usr/local/bin/eye'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,1 @@
-node.force_default['eye']['version'] = '0.6'
-node.force_default['eye']['bin'] = '/usr/local/bin/eye'
 include_recipe 'eye'
