ade32e1302d5d73a12562d9b524b99dd19d0b223
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-20 11:18:20 -0700

    Configure notifies from node attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,6 @@
 force_default['eye']['version'] = '0.6.1'
 force_default['eye']['bin'] = '/usr/local/bin/eye'
+force_default['eye']['config_template_source'] = 'config.rb.erb'
+force_default['eye']['config_template_cookbook'] = 'ace-eye'
+
+default['eye']['global'] = {}
