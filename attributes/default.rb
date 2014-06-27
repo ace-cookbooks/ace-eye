ad94524035f2650bf6de73e948101e1d34179e9a
Ryan Schlesinger <ryan@instanceinc.com>
2014-06-27 16:29:45 -0700

    Force default for first run behavior

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,3 +4,4 @@ force_default['eye']['config_template_source'] = 'config.rb.erb'
 force_default['eye']['config_template_cookbook'] = 'ace-eye'
 
 default['eye']['global'] = {}
+force_default['eye']['firstrun'] = (node['opsworks']['activity'] == 'setup')
