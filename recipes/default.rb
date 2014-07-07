10294a6b82c009e1d598c0ead262c8210299fcd6
Ryan Schlesinger <ryan@instanceinc.com>
2014-07-07 11:48:15 -0700

    Compile time no worky

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,5 @@
-# Install this at compile time for opsworks core cookbooks earlier in the run list
 gem_package 'eye-notify-hipchat' do
-  action :nothing
-end.run_action(:install)
-Gem.clear_paths
+  action :install
+end
 
 include_recipe 'eye'
