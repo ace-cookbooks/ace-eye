14c7fad6b463cbe52379f15e65bf15d85a4d4f88
Ryan Schlesinger <ryan@instanceinc.com>
2014-07-07 10:38:42 -0700

    Install eye-notify-hipchat at compile time

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,2 +1,7 @@
-gem_package 'eye-notify-hipchat'
+# Install this at compile time for opsworks core cookbooks earlier in the run list
+gem_package 'eye-notify-hipchat' do
+  action :nothing
+end.run_action(:install)
+Gem.clear_paths
+
 include_recipe 'eye'
